a84b95e12387ea934c06e619975c6db3b4802f2a
Shiv2552 <sling2552@gmail.com>
2025-06-20 14:56:18 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,9 +3,11 @@ let S2 = 0
 basic.forever(function () {
     switch1 = pins.digitalReadPin(DigitalPin.P11)
     S2 = pins.digitalReadPin(DigitalPin.P5)
-    if (switch1 == 0 || S2 == 0) {
-    	
+    if (switch1 == 1 && S2 == 0) {
+        basic.showIcon(IconNames.Heart)
+    } else if (switch1 == 0 && S2 == 1) {
+        basic.showIcon(IconNames.Happy)
     } else {
-    	
+        basic.clearScreen()
     }
 })
